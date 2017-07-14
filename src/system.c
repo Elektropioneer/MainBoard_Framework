2bda3f5cb40754fada0245d9ba15e1ebbc5b47f2
gproduct <gproduct1379@yahoo.com>
2017-07-14 09:14:33 +0200

    memcpy my only friend... 0.0

--- src/system.c
+++ src/system.c
@@ -29,13 +29,13 @@ static void (*timer_callback)(void) = NULL;
 
 unsigned int received = 0;
 
-//pin and the starting state
+//pin and the starting state of debug pins
 uint8_t debug_pins[5][2] = {
-		{48, OFF},
-		{49, OFF},
-		{50, OFF},
-		{51, OFF},
-		{52, OFF}
+		{48, OFF},					// PORTG0
+		{49, OFF},					// PORTG1
+		{50, OFF},					// PORTG2
+		{51, OFF},					// PORTG3
+		{52, OFF}					// PORTG4
 };
 
 /*
@@ -94,15 +94,23 @@ static void debug_init() {
 	int num_of_elements = ((int) (sizeof (debug_pins) / sizeof (debug_pins)[0]));
 
 	for(int i=0; i < num_of_elements; i++) {
-		gpio_register_pin(debug_pins[i][0], GPIO_DIRECTION_OUTPUT, false);
-		gpio_write_pin(debug_pins[i][0], debug_pins[i][1]);
+		gpio_register_pin(debug_pins[i][0], GPIO_DIRECTION_OUTPUT, false);				// register pin as output and no pull up
+		gpio_write_pin(debug_pins[i][0], debug_pins[i][1]);								// set the "starting" value
 	}
 }
 
-void debug_switch(uint8_t pin) 				{ 		gpio_write_pin(debug_pins[pin][0], ~debug_pins[pin][1]); 					}
-void debug_set(uint8_t pin, uint8_t state) 	{ 		debug_pins[pin][1] = state; gpio_write_pin(debug_pins[pin][0], state); 		}
+void debug_switch(uint8_t pin) {
+	uint8_t reverted_state = ~debug_pins[pin][1];
 
+	memcpy(&debug_pins[pin][1], &reverted_state, 8);
+	gpio_write_pin(debug_pins[pin][0], reverted_state);
+}
+void debug_set(uint8_t pin, uint8_t state) 	{
+	memcpy(&debug_pins[pin][1], &state, 8);
+	gpio_write_pin(debug_pins[pin][0], state);
+}
 void check_jumper(uint8_t pin) {
+
 	gpio_register_pin(pin, GPIO_DIRECTION_INPUT, false);
 
 	while(gpio_read_pin(pin)){
