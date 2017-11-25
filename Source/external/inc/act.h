217f3de9a283f8948d6d9ac02138afcc34313863
silardg <gproduct1379@yahoo.com>
2017-11-25 16:24:38 +0100

    Added ping function to actuator board

--- Source/external/inc/act.h
+++ Source/external/inc/act.h
@@ -8,6 +8,6 @@
 #ifndef SOURCE_EXTERNAL_INC_ACT_H_
 #define SOURCE_EXTERNAL_INC_ACT_H_
 
-
+unsigned char actuator_ping(void);
 
 #endif /* SOURCE_EXTERNAL_INC_ACT_H_ */
--- Source/external/src/act.c
+++ Source/external/src/act.c
@@ -4,3 +4,41 @@
 #include "act.h"
 #include <string.h>
 
+	/*
+	 * 		Protocol for actuator
+	 * 		buffer[0] - ID OF TOOL (ax, relay, mosfet)
+	 * 		buffer[1] - THE EXACT ID OF THE TOOL
+	 * 		buffer[2] - THE FUNCTION TO DO (position, move, turn on off)
+	 * 		buffer[3] - value >> 8
+	 * 		buffer[4] - value & 0xFF
+	 * 		buffer[5] - value2 >> 8
+	 * 		buffer[6] - value2 & 0xFF
+	 * 		buffer[7] - 0
+	 *
+	 */
+
+/*
+ * 	Function:    unsigned char actuator_ping(void)
+ * 	Description: Pings the actuator board
+ */
+unsigned char actuator_ping(void) {
+
+	unsigned char buffer[8];
+
+	buffer[0] = 'b';				// board
+	buffer[1] = 0;					// nothing
+	buffer[2] = 'p';				// ping
+
+	// write the data to the driver lift
+	while(CAN_Write(buffer, DRIVER_LIFT_TX_IDENTIFICATOR))
+		_delay_ms(10);
+
+	// read back
+	CAN_Read(buffer, DRIVER_LIFT_RX_IDENTIFICATOR);
+
+	// if the buffer first bit is a 1 than the ping is successfull
+	if(buffer[0] == 1)
+		return 1;
+	else
+		return 0;
+}
