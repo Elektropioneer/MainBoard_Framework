778cb6a7fec35645f5422a6d0bf399c9dd94d75f
silardg <gproduct1379@yahoo.com>
2017-11-27 15:47:27 +0100

    Added comments

--- Source/external/src/score_display.c
+++ Source/external/src/score_display.c
@@ -10,17 +10,17 @@ int current_score = 0;
 
 void update_score(int increment_score_by) {
 
-	int new_score = current_score + increment_score_by;
+	int new_score = current_score + increment_score_by;		// increment the score
 
-	uint8_t _CC = new_score / 10;
+	uint8_t _CC = new_score / 10;							// get the first two digits
 
-	uint8_t S__ = new_score / 100;
-	uint8_t _S_ = (new_score - (S__ * 100)) / 10;
-	uint8_t __S = new_score - (_CC * 10);
+	uint8_t S__ = new_score / 100;							// get the first digit
+	uint8_t _S_ = (new_score - (S__ * 100)) / 10;			// get the second digit
+	uint8_t __S = new_score - (_CC * 10);					// get the third digit
 
-	uint8_t data[3] = {S__, _S_, __S};
+	uint8_t data[3] = {S__, _S_, __S};						// combine them into an array
 
-	i2c_transmit(SCORE_DISPLAY_IDDR, data, 3);
+	i2c_transmit(SCORE_DISPLAY_IDDR, data, 3);				// transmit through i2c
 
-	current_score = new_score;
+	current_score = new_score;								// update current score
 }
