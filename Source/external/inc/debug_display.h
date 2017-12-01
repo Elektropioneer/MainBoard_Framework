4b9c04b10682f5fbf63c1a7d63d93ccf43361c2f
silardg <gproduct1379@yahoo.com>
2017-12-01 17:47:25 +0100

    Added status function

--- Source/external/inc/debug_display.h
+++ Source/external/inc/debug_display.h
@@ -11,5 +11,6 @@
 unsigned char debug_ready();
 unsigned char debug_setup_wait();
 unsigned char debug_position(int16_t x, int16_t y);
+unsigned char debug_status(float odometry_bat, float glavna_bat, unsigned char side_name, unsigned char tactic_name);
 
 #endif /* SOURCE_EXTERNAL_INC_DEBUG_DISPLAY_H_ */
